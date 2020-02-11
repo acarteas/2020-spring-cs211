b914e643e216e33d309a586e84adf4ae380811d5
Adam Carter <mail@adam-carter.com>
2020-02-11 14:19:02 -0800

    added today's lecture notes.

--- class notes/2 - vectors/02 - programming challenges/code_cpp/main.cpp
+++ class notes/2 - vectors/02 - programming challenges/code_cpp/main.cpp
@@ -12,7 +12,58 @@ vector<int> mergeSorted(vector<int>& left, vector<int>& right)
 	return result;
 }
 
-int main(void)
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+vector<int> erase_range(vector<int>& input, int start, int stop);
+vector<int> erase_single(vector<int>& start, int position)
 {
-	return 0;
+    return erase_range(start, position, position + 2);
+}
+
+vector<int> erase_range(vector<int>& input, int start, int stop)
+{
+    int end = stop;
+    for (int i = start - 1; i < input.size() && end < input.size(); i++)
+    {
+        input[i] = input[end];
+        end++;
+    }
+    while (end > stop)
+    {
+        input.pop_back();
+        end--;
+    }
+    return input;
+}
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    vector<int> input_values{};
+    int size = 0;
+    cin >> size;
+    for (int i = 0; i < size; i++)
+    {
+        int temp;
+        cin >> temp;
+        input_values.push_back(temp);
+    }
+    int first_query;
+    cin >> first_query;
+    int min_range;
+    int max_range;
+    cin >> min_range;
+    cin >> max_range;
+    vector<int> result = erase_single(input_values, first_query);
+    result = erase_range(result, min_range, max_range);
+    cout << result.size() << endl;
+    for (auto value : result)
+    {
+        cout << value << " ";
+    }
+    return 0;
 }
---
+++ class notes/3 - hash tables/code_cs/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace code_cs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<PrintJob> jobs = new List<PrintJob>();
+            foreach(var item in File.ReadLines("printer_log.csv"))
+            {
+                List<string> pieces = item.Split(',').ToList();
+                if(pieces.Count == 4)
+                {
+                    try 
+                    {
+                        PrintJob job = new PrintJob();
+                        job.Date = DateTime.Parse(pieces[0]);
+                        job.Page = Int32.Parse(pieces[1]);
+                        job.UserId = Int32.Parse(pieces[2]);
+                        job.PrinterId = Int32.Parse(pieces[3]);
+                        jobs.Add(job);
+                    }
+                    catch(Exception ex)
+                    {
+
+                    }
+                    
+                }
+            }
+
+            //in C# a HT is called a dictionary
+            Dictionary<DateTime, int> dates = new Dictionary<DateTime, int>();
+            foreach(var job in jobs)
+            {
+                if(dates.ContainsKey(job.Date) == false)
+                {
+                    dates.Add(job.Date, 0);
+                }
+                dates[job.Date] += job.Page;
+            }
+            DateTime max_date = DateTime.MinValue;
+            int max_pages = -1;
+            foreach(var kvp in dates)
+            {
+                if(kvp.Value > max_pages)
+                {
+                    max_pages = kvp.Value;
+                    max_date = kvp.Key;
+                }
+            }
+            Console.WriteLine("pages: " + max_pages + " date: " + max_date);
+        }
+    }
+}
