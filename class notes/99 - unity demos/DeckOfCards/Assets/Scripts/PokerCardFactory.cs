bd071e17db079c1364d6f602d633abea14f089a3
Adam Carter <mail@adam-carter.com>
2020-02-19 10:39:13 -0800

    Added example card project to be used in Milestone #3

---
+++ class notes/99 - unity demos/DeckOfCards/Assets/Scripts/PokerCardFactory.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PokerCardFactory
+{
+    private static PokerCardFactory _instance = null;
+    private static string _cardFacePath = "PokerCards";
+    public Dictionary<string, Material> Materials = new Dictionary<string, Material>();
+    private PokerCardFactory()
+    {
+        Object[] resources = Resources.LoadAll<Texture2D>(_cardFacePath);
+        foreach (var resource in resources)
+        {
+            Texture2D texture = resource as Texture2D;
+            if (texture != null)
+            {
+                Material material = new Material(Shader.Find("Standard"));
+                material.SetTexture("_MainTex", texture);
+                Materials.Add(resource.name, material);
+            }
+
+        }
+    }
+
+    public static PokerCardFactory GetInstance()
+    {
+        if(_instance == null)
+        {
+            _instance = new PokerCardFactory();
+        }
+        return _instance;
+    }
+
+
+
+
+}
