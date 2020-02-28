1993b9b8b199fd3387370dab949f89c57aea032a
moorcan <moorcan85@gmail.com>
2020-02-28 14:33:48 -0800

    added Shuffle and converted the variables correctly, initialized card list, added foreach loop to add cards back to Cards.

--- class notes/99 - unity demos/DeckOfCards/Assets/Scripts/Deck.cs
+++ class notes/99 - unity demos/DeckOfCards/Assets/Scripts/Deck.cs
@@ -4,7 +4,23 @@ using System.Linq;
 using UnityEngine;
 
 public class Deck 
+
 {
+    List<Card> cards = new List<Card>();
+    public int CardCount
+    {
+        get
+        {
+            if (cards == null)
+            {
+                return 0;
+            }
+            else
+            {
+                return cards.Count;
+            }
+        }
+    }
     protected LinkedList<Card> Cards { get; set; }
 
     public Deck()
@@ -27,7 +43,24 @@ public class Deck
 
     public void Shuffle()
     {
+        cards = Cards.ToList();
+        int n = cards.Count;
+        while (n > 1)
+        {
+            n--;
+            int k = Random.Range(0, n + 1);
+            Card temp = cards[k];
+            cards[k] = cards[n];
+            cards[n] = temp;
+        }
+
+        Cards.Clear();
+        foreach(Card c in cards)
+        {
+            Cards.AddFirst(c);
+        }
+
     }
 
-    
+
 }
--- class notes/99 - unity demos/DeckOfCards/Assets/Scripts/GameManager.cs
+++ class notes/99 - unity demos/DeckOfCards/Assets/Scripts/GameManager.cs
@@ -13,6 +13,8 @@ public class GameManager : MonoBehaviour
     {
         _deck = new PokerDeck();
         DrawTopCard();
+        _deck.Shuffle();
+        Debug.Log("");
         
     }
 
@@ -35,6 +37,52 @@ public class GameManager : MonoBehaviour
         }
     }
 
+    void DrawBottomCard()
+    {
+        if (CardPrefab != null)
+        {
+            var card = GameObject.Instantiate<GameObject>(CardPrefab, Vector3.zero, Quaternion.identity);
+            var controller = card.GetComponent<CardController>();
+
+            //attach card C# object to card Unity object
+            controller.Card = _deck.RemoveFromTop();
+
+            if (controller != null)
+            {
+                controller.FaceMaterial = PokerCardFactory.GetInstance().Materials[controller.Card.Name];
+                controller.UpdateFaceMaterial();
+            }
+        }
+    }
+
+    void Shuffle()
+    {
+        float Choose(float[] probs)
+        {
+
+            float total = 0;
+
+            foreach (float elem in probs)
+            {
+                total += elem;
+            }
+
+            float randomPoint = Random.value * total;
+
+            for (int i = 0; i < probs.Length; i++)
+            {
+                if (randomPoint < probs[i])
+                {
+                    return i;
+                }
+                else
+                {
+                    randomPoint -= probs[i];
+                }
+            }
+            return probs.Length - 1;
+        }
+    }
     // Update is called once per frame
     void Update()
     {
