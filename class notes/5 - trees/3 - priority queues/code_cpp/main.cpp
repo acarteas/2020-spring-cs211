372403595ccacf0dd02cda74d4041fafe5d21155
Adam Carter <mail@adam-carter.com>
2020-03-26 10:38:05 -0700

    added starter code for today's lecture.

---
+++ class notes/5 - trees/3 - priority queues/code_cpp/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <unordered_map>
+#include <utility>
+#include <stack>
+#include <queue>
+#include <algorithm>
+
+using namespace std;
+
+int main(void)
+{
+	return 0;
+}
---
+++ class notes/5 - trees/3 - priority queues/code_cs/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace code_cs
+{
+    class Program
+    {
+        static string[] GetNames()
+        {
+            var names = File.ReadAllLines("first_names.txt");
+            return names;
+        }
+
+        static void Main(string[] args)
+        {
+            var names = GetNames();
+            List<Patient> patients = new List<Patient>();
+            for(int i = 0; i < 1000; i++)
+            {
+                patients.Add(Patient.GenerateRandomPatient(names));
+            }
+        }
+    }
+}
