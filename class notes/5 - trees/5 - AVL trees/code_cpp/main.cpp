0f7966c9fcce68e0c6d87d2d91576130112cf098
Adam Carter <mail@adam-carter.com>
2020-04-03 07:57:33 -0700

    yesterday's lecture code (broken -- will fix soon)

---
+++ class notes/5 - trees/5 - AVL trees/code_cpp/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <unordered_map>
+#include <utility>
+#include <stack>
+#include <queue>
+#include <algorithm>
+#include <cstdlib>
+#include <map> //a map is a "red-black" binary tree
+#include "PrintLog.h"
+#include "StringHelpers.h"
+#include "BinarySearchTree.h"
+#include "AvlTree.h"
+
+using namespace std;
+
+int main(void)
+{
+	AvlTree<int> bst{};
+
+	for (int i = 0; i < 10; i++)
+	{
+		bst.addElement(rand() % 100);
+	}
+	bst.removeElement(41);
+	return 0;
+}
