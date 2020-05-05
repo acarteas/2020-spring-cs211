572d929587184716dfccbd2759d0a2c54f9207cf
Adam Carter <mail@adam-carter.com>
2020-05-05 13:02:04 -0700

    added today's starter code (mostly blank)

---
+++ class notes/8 - exam and interview questions/code_cpp/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <utility>
+#include <cstdlib>
+#include <ctime>
+#include <cmath>
+#include <unordered_map>
+#include <functional>
+#include <chrono>
+
+using namespace std::chrono;
+using namespace std;
+
+int main(void)
+{
+	
+	return 0;
+}
